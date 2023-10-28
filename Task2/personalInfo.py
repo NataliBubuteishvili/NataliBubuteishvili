216d9c5a14036897d8b8b9377410b1810a848330
NataliBubuteishvili <natalibubuteishvili@gmail.com>
2023-10-28 23:53:11 +0400

    Task2- Add Personal Info Collector App

---
+++ Task2/personalInfo.py
@@ -0,0 +1,12 @@
+Name = input("what is your name?: ")
+Surname = input("what is you surname?: ")
+Age = int(input("how old are you?: "))
+School = input("which school do you go to?: ")
+Place = input("where are you from?: ")
+
+print(Name, Surname, Age, School, Place)
+print("My name is "+Name+Surname)
+print("I am "+str(Age)+" years old")
+print("I am from "+Place)
+print("i study in "+School)
+
