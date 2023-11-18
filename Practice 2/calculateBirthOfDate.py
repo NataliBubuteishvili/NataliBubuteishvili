4d0969ce80b2b521fcbc74fcd4ebd29658d50b7b
NataliBubuteishvili <natalibubuteishvili@gmail.com>
2023-11-19 00:43:17 +0400

    add practice2

---
+++ Practice 2/calculateBirthOfDate.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+todaydate = date.today()
+
+birthyear = int(input("what year were you born: "))
+birthmonth = int(input("what month were you born: "))
+birthday = int(input("what day were you born: "))
+
+print("your current age is" "todaydate")
+
