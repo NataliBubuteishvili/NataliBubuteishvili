854c6edca6c2661140f5deda4149bb03ea2563c2
NataliBubuteishvili <natalibubuteishvili@gmail.com>
2024-01-28 01:48:38 +0400

    add Task five

---
+++ Task5/capitalCities.py
@@ -0,0 +1,26 @@
+countries = {
+    1: "Georgia",
+    2: "UK",
+    3: "Spain",
+    4: "Japan"
+}
+
+capitals = {
+    1: "Tbilisi",
+    2: "London",
+    3: "Madrid",
+    4: "Tokyo"
+}
+
+usernumber = int(input("Which country's capital are you interested in?: "))
+
+if usernumber == 1:
+    print(f"The capital of {countries[usernumber]} is {capitals[usernumber]}.")
+elif usernumber == 2:
+    print(f"The capital of {countries[usernumber]} is {capitals[usernumber]}.")
+elif usernumber == 3:
+    print(f"The capital of {countries[usernumber]} is {capitals[usernumber]}.")
+elif usernumber == 4:
+    print(f"The capital of {countries[usernumber]} is {capitals[usernumber]}.")
+else:
+    print("The entered number does not match the country.")
